9aed27918ed9382c2fb6796a13db9b3de2de8644
Kerbalnut <Kerbalnut@users.noreply.github.com>
2023-10-29 17:52:33 -0700

    Continued setup for Module.Template
    by following steps from https://github.com/ili101/Module.Template/blob/master/README.md

--- Invoke-ExampleCommand.ps1
+++
@@ -1,3 +0,0 @@
-function Invoke-ExampleCommand {
-    'This is an example function from "Module.Template", for more information see https://github.com/ili101/Module.Template/blob/master/README.md'
-}
